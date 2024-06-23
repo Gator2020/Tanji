5d545070f537944ae9d6d999d1bd469e587de03d
Adolfo Huitron <adolfo@huitron.dev>
2024-06-22 22:40:34 -0700

    chore: add IMiddleman interface for describing the process of intercepting packets

---
+++ Tanji.Core/Net/IMiddleman.cs
@@ -0,0 +1,10 @@
+﻿namespace Tanji.Core.Net;
+
+public interface IMiddleman
+{
+    bool IsInterceptingOutgoing { get; set; }
+    bool IsInterceptingIncoming { get; set; }
+
+    ValueTask<bool> PacketInboundAsync(Memory<byte> buffer, HNode source, HNode destination);
+    ValueTask<bool> PacketOutboundAsync(Memory<byte> buffer, HNode source, HNode destination);
+}
